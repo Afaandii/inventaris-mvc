3b64a0749a329ea7b2741b6c3d722fba010666f0
Afaandii <fandisantuy140@gmail.com>
2025-06-23 17:38:40 +0700

    inisialisasi js siswa

---
+++ public/js/siswa.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $('#peminjam').on('change', function () {
+        var idPeminjam = $(this).val();
+        
+        $.ajax({
+            type: 'POST',
+            url: 'http://localhost/inventaris_mvc/public/siswa/getKelasSiswaByPeminjam',
+            data: { id_peminjam: idPeminjam },
+            success: function (data) {
+                $('#kelsis').html(data);
+            },
+            error: function (xhr, status, error) {
+                console.error("Error: ", error);
+            }
+        });
+    });
+});

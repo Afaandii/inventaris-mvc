a933f0d267d58f30dbe2b46f6587d42e74ea4abe
Afaandii <fandisantuy140@gmail.com>
2025-06-24 20:11:26 +0700

    revisi ui kelas

--- App/view/kelas/edit.php
+++ App/view/kelas/edit.php
@@ -4,7 +4,7 @@
     <div class="container-fluid">
       <div class="row mb-2">
         <div class="col-sm-6">
-          <h1>Update Kelas</h1>
+          <h1 class="font-weight-bold">Form Edit Kelas</h1>
         </div>
       </div>
     </div><!-- /.container-fluid -->
@@ -25,7 +25,9 @@
               <div class="card-body">
                 <div class="form-group">
                   <label for="kode">Kode Kelas</label>
-                  <input type="text" name="kode" id="kode" value="<?= $data['kelas']['KODE_KELAS'] ?>" readonly> <br>
+                  <input class="form-control mb-2" type="text" name="kode" id="kode"
+                    value="<?= $data['kelas']['KODE_KELAS'] ?>" readonly>
+
                   <label for="kelas">Kelas</label>
                   <input type="text" name="kelas" class="form-control" id="kelas" placeholder="Masukan kelas"
                     value="<?php echo $data['kelas']["NAMA_KELAS"]; ?>">
@@ -33,7 +35,8 @@
               </div>
               <!-- /.card-body -->
               <div class="card-footer">
-                <button type="submit" name="submit" class="btn btn-primary">Edit Kelas</button>
+                <button type="submit" name="submit" class="btn btn-primary">Simpan</button>
+                <a href="<?= BASEURL ?>/kelas" class="btn btn-info float-md-right">Kembali</a>
               </div>
             </form>
           </div>
--- App/view/kelas/kelas.php
+++ App/view/kelas/kelas.php
@@ -3,8 +3,9 @@
   <div class="content-header">
     <div class="container-fluid">
       <div class="row mb-2">
-        <div class="col-sm-6">
-          <h1 class="m-0">Tabel Kelas</h1>
+        <div class="col-sm-12 d-flex justify-content-between align-items-center">
+          <h1 class="m-0">Manage Tabel Kelas</h1>
+          <a href="<?= BASEURL ?>/kelas/create" class="btn btn-secondary">Tambah Kelas</a>
         </div><!-- /.col -->
       </div><!-- /.row -->
     </div><!-- /.container-fluid -->
@@ -38,21 +39,21 @@
                     <tbody>
                       <?php $i = 1; ?>
                       <?php foreach ($data['kelas'] as $kel) { ?>
-                      <tr>
-                        <td><?php echo $i; ?></td>
-                        <td>
-                          <a href="<?= BASEURL ?>/kelas/edit/<?php echo $kel["ID_KELAS"]; ?>">Update</a> |
-                          <a href="<?= BASEURL ?>/kelas/delete/<?php echo $kel["ID_KELAS"]; ?>"
-                            onclick="return confirm('Apakah anda yakin menghapus data?')">Delete</a>
-                        </td>
-                        <td><?= $kel["KODE_KELAS"]; ?></td>
-                        <td><?php echo $kel["NAMA_KELAS"]; ?></td>
-                      </tr>
-                      <?php $i++; ?>
+                        <tr>
+                          <td><?php echo $i; ?></td>
+                          <td>
+                            <a class="btn btn-warning mr-2"
+                              href="<?= BASEURL ?>/kelas/edit/<?php echo $kel["ID_KELAS"]; ?>">Update</a>
+                            <a class="btn btn-danger" href="<?= BASEURL ?>/kelas/delete/<?php echo $kel["ID_KELAS"]; ?>"
+                              onclick="return confirm('Apakah anda yakin menghapus data?')">Delete</a>
+                          </td>
+                          <td><?= $kel["KODE_KELAS"]; ?></td>
+                          <td><?php echo $kel["NAMA_KELAS"]; ?></td>
+                        </tr>
+                        <?php $i++; ?>
                       <?php }; ?>
                     </tbody>
                   </table>
-                  <a href="<?= BASEURL ?>/kelas/create">Tambah Kelas</a>
                 </div>
                 <!-- /.card-body -->
               </div>

66869108f35ff4331276f684e7b0e91b7e7cea02
Afaandii <fandisantuy140@gmail.com>
2025-06-27 19:11:15 +0700

    remember me cookie login

--- App/controller/Auth.php
+++ App/controller/Auth.php
@@ -1,6 +1,18 @@
 <?php
 class Auth extends Controller
 {
+  public function __construct()
+  {
+    if (!isset($_SESSION['user']) && isset($_COOKIE['username']) && isset($_COOKIE['token'])) {
+      $auth = $this->model('Auth_model')->login($_COOKIE['username']);
+      if ($auth && hash('sha256', $auth['PASSWORD_PEMINJAM']) === $_COOKIE['token']) {
+        $_SESSION['user'] = $auth;
+        header("Location: " . BASEURL . "/home");
+        exit;
+      }
+    }
+  }
+
   public function index()
   {
     $this->view('template/auth_header');
@@ -19,12 +31,17 @@ class Auth extends Controller
   {
     $username = $_POST['username'];
     $password = $_POST['password'];
+    $remember = isset($_POST['remember']);
 
     $auth = $this->model('Auth_model')->login($username);
 
     if ($auth) {
       if ($auth['PASSWORD_PEMINJAM'] == $password) {
         $_SESSION['user'] = $auth;
+        if ($remember) {
+          setcookie('username', $username, time() + (86400 * 30), "/"); // 30 hari
+          setcookie('token', password_hash($auth['PASSWORD_PEMINJAM'], PASSWORD_BCRYPT), time() + (86400 * 30), "/");
+        }
         header("Location: " . BASEURL . "/home");
         die;
       } else {
@@ -38,6 +55,8 @@ class Auth extends Controller
   public function logout()
   {
     session_destroy();
+    setcookie('username', '', time() - 3600, "/");
+    setcookie('token', '', time() - 3600, "/");
     header("Location: " . BASEURL . "/auth");
   }
 }
